08cc484166e8b21b102765282a77752b262be7d7
keson2625 <wjdgns2625@naver.com>
2025-04-08 20:50:53 +0900

    찐찐찐 최종본

--- src/main/java/kr/kh/team3/controller/UserController.java
+++ src/main/java/kr/kh/team3/controller/UserController.java
@@ -9,16 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.kh.team3.model.vo.MemberVO;
 import kr.kh.team3.service.UserService;
 import lombok.extern.log4j.Log4j;
 
+@RequestMapping("/user")
 @Log4j
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
 	
@@ -30,7 +32,7 @@ public class UserController {
 	    return "/user/myPage";
 	}
 	
-	@GetMapping("/user/loadData")
+	@GetMapping("/loadData")
 	public String loadData(@RequestParam("type") String type, Model model, HttpSession session) {
 	    // 세션에서 로그인 유저 꺼냄
 	    MemberVO user = (MemberVO) session.getAttribute("member");
@@ -44,5 +46,10 @@ public class UserController {
 	    } else {
 	        return "redirect:/myPage"; 
 	    }
+	}
+	
+	@GetMapping("/apply")
+	public String applyCreator() {
+		return "/member/apply";
 	}
 }
